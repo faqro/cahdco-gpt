62c833859d9d77be6fbd3e7fdfb869524bcad2d2
Faraaz Jan <faraaz.jan@gmail.com>
2024-08-26 15:27:00 -0400

    Implemented recursive RAG document setup using EmbedJS
    Program now supports multi-file RAG and automatically adds all files in the 'docs' folder to EmbedJS for RAG Q/A

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# cahdco-gpt
+# CahdcoGPT
  
+RAG LLM processing tool for enterprise documentation
+Designed for [[cahdco.org]]
+
+Simple question/answering from a limited dataset
+
+Built by Faiz Jan and Faraaz Jan
+Using LanceDB and EmbedJS
+
+## Instructions
+1. Run `npm install`
+2. Place relevant documents to add to RAG in a `docs` folder in the main directory
+3. Put OpenAI API Key in `.env` file in main directory
+4. Run `npm start`
--- app.js
+++ app.js
@@ -1,41 +1,72 @@
-
-
-const express = require('express')
-require('express-async-errors')
+import path from 'path'
+import fs from 'fs'
+import express from 'express'
+import 'express-async-errors'
 const app = express()
-const cors = require('cors')
+import cors from 'cors'
+import 'dotenv/config'
 
-const officeParser = require('officeparser')
-
-const { OpenAI } = require('openai')
-const openai = new OpenAI({ apiKey: openAiKey })
+import { RAGApplicationBuilder, DocxLoader, OpenAi, PdfLoader, ExcelLoader, PptLoader } from '@llm-tools/embedjs'
+import { LanceDb } from '@llm-tools/embedjs/vectorDb/lance'
 
 app.use(cors())
 app.use(express.json())
 
+const DOCUMENT_DIRECTORY = "docs"
 
+let ragApplication = null
+new RAGApplicationBuilder()
+    .setModel(new OpenAi({ modelName: 'gpt-4o-mini' }))
+    .setVectorDb(new LanceDb({ path: path.resolve('lancedb') }))
+    .setSearchResultCount(5)
+    .build()
+    .then(async(result) => {
+        const filesToLoop = addLoadersRecursive(DOCUMENT_DIRECTORY)
+        let unmappedFiles = []
 
-app.get('/main', async(req, res) => {
-    const parsedDocument = await officeParser.parseOfficeAsync('docs/document.docx')
-    console.log(`Parsed ${parsedDocument.length} characters in document`)
-
-    console.log('Generating response')
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                "role": "system",
-                "content": `answer the user's questions using the information below:\n${parsedDocument}`
-            },
-            {
-                "role": "user",
-                "content": req.body.message
+        filesToLoop.forEach(({fileDir, fileType}) => {
+            if(fileType === "docx") {
+                result.addLoader(new DocxLoader({filePathOrUrl: fileDir}))
+            } else if(fileType === "pdf") {
+                result.addLoader(new PdfLoader({filePathOrUrl: fileDir}))
+            } else if(fileType === "xlsx") {
+                result.addLoader(new ExcelLoader({filePathOrUrl: fileDir}))
+            } else if(fileType === "pptx") {
+                result.addLoader(new PptLoader({filePathOrUrl: fileDir}))
+            } else {
+                unmappedFiles = unmappedFiles.concat(fileDir)
             }
-        ]
-    })
-    console.log('Returned LLM response')
+        })
 
-    return res.status(200).json(completion.choices[0].message)
+        console.log(`Mapped ${filesToLoop.length - unmappedFiles.length} files`)
+        console.log(`${unmappedFiles.length} unmapped files`, unmappedFiles)
+
+        ragApplication = result
+    })
+
+const addLoadersRecursive = (dir) => {
+    let files = []
+    fs.readdirSync(dir).forEach(File => {
+        const Absolute = path.join(dir, File)
+        if (fs.statSync(Absolute).isDirectory()) return addLoadersRecursive(Absolute)
+        else return files.push(Absolute)
+    })
+    return files.map(fileDir => ({fileDir, fileType: fileDir.split('.').pop().toLowerCase() }))
+}
+
+
+app.get('/rag', async(req, res) => {
+    if(ragApplication == null) {
+        return res.sendStatus(425)
+    }
+
+    try {
+        const response = await ragApplication.query(req.body.message)
+
+        return res.status(200).json(response)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
 })
 
-module.exports = app
+export {app}
